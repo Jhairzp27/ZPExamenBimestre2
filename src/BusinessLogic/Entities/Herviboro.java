f06381a95fe42c48b03bb5bf47e04369b069fd16
Jhairzp_27 <jhairzp27@hotmail.com>
2024-02-29 13:57:09 -0500

    Agrego documentacion

--- src/BusinessLogic/Entities/Herviboro.java
+++ src/BusinessLogic/Entities/Herviboro.java
@@ -1,3 +1,12 @@
+/*
+|----------------------------------------|
+| (©) 2K24 EPN-FIS, All rights reserved. |
+| pdavila0113@gmail.com      BaphomeT-T  |
+|----------------------------------------|
+Autor : BaphomeT-T
+Fecha : 25.feb.2k24
+*/
+
 package BusinessLogic.Entities;
 
 public class Herviboro extends ZPAlimento{
